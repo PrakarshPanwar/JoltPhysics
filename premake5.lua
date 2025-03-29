6ef9a4defa489c9d4643a43466e7d074d32ab97b
PrakarshPanwar <prakarshpanwar@gmail.com>
2025-03-29 19:16:11 +0530

    Modified .gitignore
    - Added Custom premake

---
+++ premake5.lua
@@ -0,0 +1,35 @@
+project "JoltPhysics"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"Jolt/**.cpp",
+		"Jolt/**.h"
+	}
+
+	includedirs {
+		"%{prj.location}"
+    }
+
+    filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++20"
+		staticruntime "Off"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "Off"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+        defines { "NDEBUG", "JPH_DEBUG" }
+		runtime "Release"
+		optimize "on"
